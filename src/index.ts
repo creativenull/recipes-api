2089208098b2c82ce60c21352af3b8b8d100dfed
Arnold Chand <creativenull@outlook.com>
2021-04-17 21:06:04 -0400

    feat: use oak as http server/middleware

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,31 @@
-import { App } from 'tinyhttp'
-import { cors } from 'tinyhttp_cors'
-import { GraphQLHTTP } from 'gql'
+// import { App } from 'tinyhttp'
+// import { cors } from 'tinyhttp_cors'
+// import { applyGraphQL } from 'oak_graphql'
+// import { GraphQLHTTP } from 'gql'
+import { Application, Router } from 'oak'
+import { oakCors } from 'oak_cors'
+import graphqlHTTP from 'koa_graphql'
 import { schema, root } from './gql/schema.ts'
 
-const app = new App()
+const app = new Application()
 
-app.use(cors())
+app.addEventListener('listen', (ctx) => {
+  console.log('Listening on port:', ctx.port)
+})
 
-app.post('/graphql', GraphQLHTTP({ schema, rootValue: root }))
+const router = new Router()
 
-app.listen(8080, () => { console.log('Listening on port 8080') })
+router.all('/graphql', graphqlHTTP({
+  schema,
+  rootValue: root,
+  graphiql: true
+}))
+
+app.use(oakCors())
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+// app.get('/', () => { console.log('received') })
+// app.post('/graphql', GraphQLHTTP({ schema, rootValue: root }))
+await app.listen({ port: 8080 })
